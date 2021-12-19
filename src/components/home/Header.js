4ea0f6d375d5ba1c235fd77c96c3fbe68042e5ed
chisainty <johnchinedudivine@gmail.com>
2021-12-19 12:33:16 -0800

    made the slider responsive

--- src/components/home/Header.js
+++ src/components/home/Header.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 const Header = () => {
   return (
     <header className="header">
--- src/components/home/Slider.js
+++ src/components/home/Slider.js
@@ -1,7 +1,14 @@
 import { Swiper, SwiperSlide } from "swiper/react";
+import { useState, useRef, useEffect } from "react";
 import data from "../../data";
 import Slide from "./Slide";
-import SwiperCore, { Pagination, Scrollbar, Navigation, A11y } from "swiper";
+import SwiperCore, {
+  Pagination,
+  Scrollbar,
+  Navigation,
+  A11y,
+  Autoplay,
+} from "swiper";
 
 // install Swiper modules
 // SwiperCore.use([Pagination]);
@@ -14,14 +21,44 @@ import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 import "swiper/css/autoplay";
 
+SwiperCore.use([Autoplay]);
+
 const Slider = () => {
+  const [direction, setDirection] = useState("");
+  const [width, setWidth] = useState(window.innerWidth);
+  const [height, setHeight] = useState(window.innerHeight);
+
+  const updateWidth = () => {
+    setWidth(window.innerWidth);
+    // setHeight(window.innerHeight);
+    console.log(window.innerWidth);
+  };
+
+  useEffect(() => {
+    if (width > 600) {
+      setDirection("vertical");
+    } else {
+      setDirection("horizontal");
+    }
+  }, [width]);
+
+  useEffect(() => {
+    window.addEventListener("resize", updateWidth);
+    return () => window.removeEventListener("resize", updateWidth);
+  });
+
+  console.log(direction);
+
   return (
     <div>
       <Swiper
         modules={[Navigation, Pagination, Scrollbar, A11y]}
-        autoplay
-        direction={"vertical"}
-        navigation
+        autoplay={{ delay: 2000, disableOnInteraction: false }}
+        direction={direction}
+        navigation={{
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        }}
         pagination={{ clickable: true }}
         scrollbar={{ draggable: true }}
         className="mySwiper"
