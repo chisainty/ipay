b2fa888f8e76f3f40bff9bf0a9ebbcfc8a278ed7
chisainty <johnchinedudivine@gmail.com>
2021-12-19 12:43:21 -0800

    worked on menu toggle func

--- src/components/home/Header.js
+++ src/components/home/Header.js
@@ -1,38 +1,56 @@
 import { useState } from "react";
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [dropDownDisplay, setDropDownDisplay] = useState("d-none");
+
+  const updateMenuStatus = () => {
+    if (menuOpen) {
+      setMenuOpen(false);
+      setDropDownDisplay("");
+    } else {
+      setMenuOpen(true);
+      setDropDownDisplay("d-none");
+    }
+  };
+
   return (
     <header className="header">
-    <div className="container">
-      <div className="logo">
-        <img src="images/logo/iPay.png" width={110} alt="" />
-      </div>
-      <div className="menu d-md-none">
-        <img src="images/icons/menu.png" height={20} alt="" />
-      </div>
-      <div className="showcase d-md-flex">
-        <div className="list">
-          <ul className="d-md-flex mt-2 mb-0">
-            <li className="mx-md-4">
-              <a href="#">Save</a>
-            </li>
-            <li className="mx-md-4">
-              <a href="#">Invest</a>
-            </li>
-            <li className="mx-md-4">
-              <a href="#">Loan</a>
-            </li>
-            <li className="mx-md-4">
-              <a href="#">FAQ</a>
-            </li>
-          </ul>
+      <div className="container">
+        <div className="logo">
+          <img src="images/logo/iPay.png" width={110} alt="" />
         </div>
-        <div className="btns">
-          <a href="#" className="btn btn-outline btn-primary">Log In</a>
-          <a href="#" className="btn btn-primary ml-4"> Sign Up, It's Free</a>
+        <div className="menu d-md-none" onClick={updateMenuStatus}>
+          <img src="images/icons/menu.png" height={20} alt="" />
+        </div>
+        <div className={"showcase " + dropDownDisplay + " d-md-flex"}>
+          <div className="list">
+            <ul className="d-md-flex mt-2 mb-0">
+              <li className="mx-md-4">
+                <a href="#">Save</a>
+              </li>
+              <li className="mx-md-4">
+                <a href="#">Invest</a>
+              </li>
+              <li className="mx-md-4">
+                <a href="#">Loan</a>
+              </li>
+              <li className="mx-md-4">
+                <a href="#">FAQ</a>
+              </li>
+            </ul>
+          </div>
+          <div className="btns">
+            <a href="#" className="btn btn-outline btn-primary">
+              Log In
+            </a>
+            <a href="#" className="btn btn-primary ml-4">
+              {" "}
+              Sign Up, It's Free
+            </a>
+          </div>
         </div>
       </div>
-    </div>
-  </header>
+    </header>
   );
 };
 
